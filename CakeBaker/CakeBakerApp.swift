13b275f31b2ff335bd291b53a33f30f4cc1b63f7
Khawlah Khalid <khawlah1920@hotmail.com>
2024-09-09 13:48:17 +0300

    Initial Commit

---
+++ CakeBaker/CakeBakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CakeBakerApp.swift
+//  CakeBaker
+//
+//  Created by Khawlah Khalid on 09/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct CakeBakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
